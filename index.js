97803bfe80fd88aa9218005cf497ffd9a79729ec
scalerjo <joseph.scalera1@gmail.com>
2021-09-22 18:01:18 +0000

    switch from requests to axios

--- index.js
+++ index.js
@@ -11,101 +11,100 @@ class ObjectStorageClient {
         this.url = config.URL;
         this.test_filename = new RegExp('^[A-Za-z0-9]+[A-Za-z0-9.-]+[A-Za-z0-9]+$');
         this.test_url = new RegExp('^(http|https):\/\/[0-0a-z.]*(:[0-9]*){0,1}$');
+
         if (!this.test_url.test(this.url)){
             throw("Error: bad url format. " + this.url);
         }
     }
 
     async getNamespaceFiles(namespace) {
-        const headers = {
-            "authorization": this.api_key
-        };
-        const options = {
-            "url": this.url + "/" + namespace,
-            "headers": headers
-        }
         return new Promise(function (resolve, reject) {
-            try {
-                request.get(options, function(err, res, body){
-                    if (err) {
-                        reject(err);
-                        return;
-                    }
-                    if (res.statusCode == 200){
-                        resolve(JSON.parse(body));
-                    }
-                    else {
-                        resolve(body);
-                    }
-                });
-            }
-            catch {
+            axios({
+                url: this.url + "/" + namespace,
+                method: "GET",
+                headers: {
+                    "authorization": this.api_key
+                }
+            }).then(function(response) {
+                resolve(response.data);
+            }).catch(function(err) {
+                resolve(err.response.data)
+            });
 
-            }
         }.bind(this));
     }
 
     async putNamespace(namespace) {
-        const headers = {
-            "authorization": this.api_key
-        };
-        const options = {
-            "url": this.url + "/" + namespace,
-            "headers": headers
-        }
         return new Promise(function (resolve, reject) {
-            request.put(options, function(err, res, body){
-                resolve(body);
-                reject(err);
+            axios({
+                url: this.url + "/" + namespace,
+                method: "PUT",
+                headers: {
+                    "authorization": this.api_key
+                }
+            }).then(function(response) {
+                resolve(response.data);
+            }).catch(function(err) {
+                resolve(err.response.data)
             });
+
         }.bind(this));
     }
 
     async deleteNamespace(namespace) {
-        const headers = {
-            "authorization": this.api_key
-        };
-        const options = {
-            "url": this.url + "/" + namespace,
-            "headers": headers
-        }
         return new Promise(function (resolve, reject) {
-            request.delete(options, function(err, res, body){
-                resolve(body);
+            axios({
+                url: this.url + "/" + namespace,
+                method: "DELETE",
+                headers: {
+                    "authorization": this.api_key
+                }
+            }).then(function(response) {
+                resolve(response.data);
+            }).catch(function(err) {
+                resolve(err.response.data)
             });
         }.bind(this));
     }
 
     async getObject(namespace, file_name, download_path) {
-        const headers = {
-            "authorization": this.api_key
-        };
-        const options = {
-            "url": this.url + "/" + namespace + "/" + file_name,
-            "headers": headers
-        }
-        return new Promise(function (resolve, reject) {
-            const r = request.get(options, function(err, res, body){
-                if (res.statusCode == 400) {
-                    resolve(body);
-                    return;
+
+        return new Promise(async function (resolve, reject) {
+
+
+            axios({
+                url: this.url + "/" + namespace + "/" + file_name,
+                method: "GET",
+                headers: {
+                    "authorization": this.api_key
+                },
+                responseType: 'stream'
+            }).then(function(response){
+                var file = fs.createWriteStream(path.join(download_path, file_name));
+
+                // close() is async, call cb after close completes
+                file.on('finish', function(err, res ,body){
+                    file.close();
+                    resolve("Success!");
+                });
+                
+                response.data.pipe(file);
+            }).catch(function(err) {
+                const statusCode = err.response.status;
+                if (statusCode == 400) {
+                    resolve("Error: Unauthorized!");
                 }
-            });
-    
-            
-    
-            // verify response code
-            r.on('response', (response) => {
-                if (response.statusCode < 400) {
-                    var file = fs.createWriteStream(path.join(download_path, file_name));
-        
-                    // close() is async, call cb after close completes
-                    file.on('finish', function(err, res ,body){
-                        file.close();
-                        resolve("Success!");
-                    });
-                    
-                    r.pipe(file);
+                else if (statusCode == 401) {
+                    resolve("Error: object is write locked");
+                }
+                else if (statusCode == 402) {
+                    resolve("Error: namespace doesn't exist");
+                }
+                else if (statusCode == 403) {
+                    resolve("Error: object doesn't exist");
+                }
+                else {
+                    resolve("Error: unknown error");
                 }
             });
         }.bind(this));
@@ -116,9 +115,12 @@ class ObjectStorageClient {
         const formData = new FormData();
         formData.append('file', fs.createReadStream(file_path));
         return new Promise(function (resolve, reject) {
-            axios.put(this.url + "/" + namespace + "/" + file_name, formData, {
+            axios({
+                url: this.url + "/" + namespace + "/" + file_name,
+                method: "PUT",
                 maxContentLength: Infinity,
                 maxBodyLength: Infinity,
+                data: formData,
                 headers: {
                     "content-type": formData.getHeaders()['content-type'],
                     "authorization": this.api_key
@@ -132,16 +134,17 @@ class ObjectStorageClient {
     }
 
     deleteObject(namespace, file_name) {
-        const headers = {
-            "authorization": this.api_key
-        };
-        const options = {
-            "url": this.url + "/" + namespace + "/" + file_name,
-            "headers": headers
-        }
         return new Promise(function (resolve, reject) {
-            request.delete(options, function(err, res, body){
-                resolve(body);
+            axios({
+                url: this.url + "/" + namespace + "/" + file_name,
+                method: "DELETE",
+                headers: {
+                    "authorization": this.api_key
+                }
+            }).then(function(response) {
+                resolve(response.data);
+            }).catch(function(err) {
+                resolve(err.response.data)
             });
         }.bind(this))
     }
